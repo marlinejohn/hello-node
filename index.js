9908921a357f88f6518df1404e86f69042409cf5
Marline <marlinejohn@gmail.com>
2025-06-11 00:03:20 +0200

    Update greeting

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get('/healthcheck', (req, res) => {
-    res.send("It works!");
+app.get("/healthcheck", (req, res) => {
+  res.send("It works!Node works !!");
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
-
